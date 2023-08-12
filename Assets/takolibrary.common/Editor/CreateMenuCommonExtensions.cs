3833c2b91e8f7d174143434432b0ac6f6ed5da0c
DESKTOP-A75HLA4\takohachi <takoroku666@gmail.com>
2023-08-12 23:37:57 +0900

    [add] CreateMenu extensions

---
+++ Assets/takolibrary.common/Editor/CreateMenuCommonExtensions.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TakoLibrary.Common.Editor;
+using UnityEditor;
+
+public class CreateMenuCommonExtensions : CreateMenuExtensions<CreateMenuCommonExtensions>
+{
+    [MenuItem(MenuItemRoot+"C# Script (MonoBehaviour Basic)", false, 80)]
+    private static void CreateMonoBehaviourBasic() => CreateFile(FileType.Cs, "MonoBehaviourBasic", "new Custom MonoBehaviour Basic");
+
+    [MenuItem(MenuItemRoot + "C# Script (MonoBehaviour Advanced)", false, 80)]
+    private static void CreateMonoBehaviourAdvanced() => CreateFile(FileType.Cs, "MonoBehaviourAdvanced", "new Custom MonoBehaviour Advanced");
+}
---
+++ Assets/takolibrary.common/Editor/CreateMenuExtensions.cs
@@ -0,0 +1,77 @@
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace TakoLibrary.Common.Editor
+{
+    /// <summary>
+    /// Createメニューに項目を追加する。
+    /// テンプレートファイルはこのC#スクリプトと同じディレクトリに置く。
+    /// ScriptableObjectを継承しているのはテンプレートファイルを取得する際の実装上の都合によるもので、実際にScriptableObjectとして用いるわけではない。
+    /// </summary>
+    public class CreateMenuExtensions<T> : ScriptableObject where T : CreateMenuExtensions<T>
+    {
+
+        protected const string MenuItemRoot = "Assets/Create/";
+
+        /// <summary>
+        /// Createメニューの階層（シェーダー）。
+        /// </summary>
+        protected const string MenuItemShaderRoot = "Assets/Create/Shader/";
+
+        /// <summary>
+        /// Createメニューの階層（C#）。
+        /// </summary>
+        protected const string MenuItemCsRoot = "Assets/Create/Custom C# Scripts/";
+
+        protected enum FileType
+        {
+            Shader, // シェーダーファイル。
+            Cs, // C#スクリプト。
+        }
+
+        /// <summary>
+        /// FileTypeに対応する拡張子。
+        /// </summary>
+        protected static readonly Dictionary<FileType, string> ExtensionConversion = new()
+        {
+            [FileType.Shader] = "shader",
+            [FileType.Cs] = "cs",
+        };
+
+        /// <summary>
+        /// このC#スクリプトファイルがあるディレクトリを取得する。
+        /// </summary>
+        /// <returns>このC#スクリプトファイルのディレクトリ</returns>
+        private static string GetFilePath()
+        {
+            //C#ではC#ファイルのパスを取得する方法は無い。
+            //そこでやや強引だがUnityのScriptableObjectの機能を利用して取得する。
+            ScriptableObject instance = ScriptableObject.CreateInstance(typeof(T));
+            MonoScript monoScript = MonoScript.FromScriptableObject(instance);
+            string scriptPath = AssetDatabase.GetAssetPath(monoScript);
+
+            DestroyImmediate(instance);
+
+            return Path.GetDirectoryName(scriptPath);
+        }
+
+
+        /// <summary>
+        /// ファイルを生成する。
+        /// </summary>
+        /// <param name="fileType">ファイルの種類</param>
+        /// <param name="fileName">ファイルの名前</param>
+        /// <param name="defaultNewFileName">生成されるファイルのデフォルトの名前</param>
+        protected static void CreateFile(FileType fileType, string fileName, string defaultNewFileName)
+        {
+
+            string extension = ExtensionConversion[fileType];
+            string templatePath = $"{GetFilePath()}/{fileName}.{extension}.txt";
+            string defaultNewFileFullName = $"{defaultNewFileName}.{extension}";
+
+            ProjectWindowUtil.CreateScriptAssetFromTemplateFile(templatePath, defaultNewFileFullName);
+        }
+    }
+}
