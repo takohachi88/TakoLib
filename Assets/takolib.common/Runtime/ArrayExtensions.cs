117a1dd4c19140a330b3ccbd6e3cc93d6630d769
DESKTOP-A75HLA4\takohachi <takoroku666@gmail.com>
2023-09-25 02:27:17 +0900

    [fix] ArrayExtensions

--- Assets/takolib.common/Runtime/ArrayExtensions.cs
+++ Assets/takolib.common/Runtime/ArrayExtensions.cs
@@ -5,13 +5,15 @@ namespace TakoLib.Common.Extensions
 {
     public static class CollectionExtensions
     {
-        public static bool IsNullOrEmpty<T>(this ICollection<T> self) => self == null || self.Count == 0;
+        public static bool IsNullOrEmpty<T>(this IReadOnlyCollection<T> self) => self == null || self.Count == 0;
+        public static bool IsNullOrEmpty<T>(this IReadOnlyList<T> self) => self == null || self.Count == 0;
 
         public static void Swap<T>(this IList<T> self, int i, int j)
         {
             (self[i], self[j]) = (self[j], self[i]);
         }
 
-        public static bool IsInRange<T>(this IList<T> self, int index) => index.IsInRange(0, self.Count - 1);
+        public static bool IsInRange<T>(this IReadOnlyCollection<T> self, int index) => index.IsInRange(0, self.Count - 1);
+        public static bool IsInRange<T>(this IReadOnlyList<T> self, int index) => index.IsInRange(0, self.Count - 1);
     }
 }
