2ab2e5f6705612b6a4019d349831c8f2ddd73e1b
DESKTOP-A75HLA4\takohachi <takoroku666@gmail.com>
2025-06-09 04:07:25 +0900

    [update]Spaceオプション

--- Packages/takolib.common/Runtime/Rotator.cs
+++ Packages/takolib.common/Runtime/Rotator.cs
@@ -5,10 +5,11 @@ namespace TakoLib.Common
     public class Rotator : MonoBehaviour
     {
         [SerializeField] private Vector3 _rotation;
+        [SerializeField] private Space _space = Space.World;
 
         private void Update()
         {
-            transform.Rotate(_rotation * Time.deltaTime * 10f);
+            transform.Rotate(_rotation * Time.deltaTime * 10f, _space);
         }
     }
 }
