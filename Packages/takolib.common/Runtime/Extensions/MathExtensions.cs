c194d761917c7a66a94133ecfbaea5b486a3cea9
tako <tako>
2026-07-04 07:14:29 +0900

    高速偶奇判定

--- Packages/takolib.common/Runtime/Extensions/MathExtensions.cs
+++ Packages/takolib.common/Runtime/Extensions/MathExtensions.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using Unity.Mathematics;
 using UnityEngine;
 
@@ -7,5 +8,8 @@ namespace TakoLib.Common.Extensions
     {
         public static float4 ToFloat4(this Color self) => new(self.r, self.g, self.b, self.a);
         public static float3 ToFloat3(this Color self) => new(self.r, self.g, self.b);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static bool IsOdd(this int self) => (self & 1) != 0;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static bool IsEven(this int self) => (self & 1) == 0;
     }
 }
